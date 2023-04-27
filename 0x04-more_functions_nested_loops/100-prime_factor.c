8c6afa5879fbf0800aade1394cac4105d8a779be
blessing oladimeji <essiblessing1@gmail.com>
2023-04-27 13:52:53 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,6 +7,7 @@
 * Return: Always 0 (Success)
 */
 int main(void)
+
 {
 long int n;
 long int max;
@@ -14,13 +15,11 @@ long int i;
 
 n  612852475143;
 max = -1;
-
 while (n % 2 == 0)
 {
 max = 2;
 n /= 2;
 }
-
 for (i = 3; i <= sqrt(n); i = i + 2)
 {
 while (n % i == 0)
